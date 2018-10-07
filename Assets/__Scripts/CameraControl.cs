dafd859cef7b36cffae97a56528e3e394025f1f3
Mari Kyle <marimkyle@gmail.com>
2018-10-07 09:39:30 -0700

    Added texture to cockpit and new cockpit model.

---
+++ Assets/__Scripts/CameraControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    Transform lookAt;
+    Vector3 startoffset;
+    Vector3 moveVector;
+	// Use this for initialization
+	void Start () {
+        lookAt=GameObject.FindGameObjectWithTag("Player").transform;
+        startoffset = transform.position - lookAt.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = lookAt.position + startoffset;
+        //moveVector.x = 0;
+		moveVector.y = Mathf.Clamp(moveVector.y,lookAt.position.y,lookAt.position.y+5.5f);
+        transform.position = moveVector;
+	}
+}
---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private CharacterController controller;
+    private Animator animator;
+
+    private bool forward = true;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.UpArrow)) {
+            if(forward) {
+                SetSpeedAnimation(1);
+                PlayAnimation("Run");
+
+                forward = false;
+            }
+            else {
+                SetSpeedAnimation(-1);
+                PlayAnimation("Run");
+
+                forward = true;
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow)) {
+            PlayAnimation("Idle");
+        }
+	}
+    private void SetSpeedAnimation(float speedAnimation) {
+        animator.SetFloat("Direction",speedAnimation);
+    }
+    private void PlayAnimation(string animationToPlay) {
+        animator.SetTrigger(animationToPlay);
+    }
+}
