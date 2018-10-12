2f4c58a920363c6f5a07e234a1427e76b5aa9235
Mari Kyle <marimkyle@gmail.com>
2018-10-11 22:19:49 -0700

    Added Opening Scene
    Added Opening Scene + Opening scene assets including: Skybox, Launch pad, Shuttle, Fire Effects, music, post effects, scripts

---
+++ Assets/Opener/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject Lights1;
+    public GameObject Lights2;
+    public GameObject Lights3;
+    public GameObject Jets;
+    public GameObject Title;
+    public GameObject Shuttle;
+
+
+
+    void Start()
+    {
+
+        StartCoroutine(LateCall());
+    }
+
+    IEnumerator LateCall()
+    {
+        yield return new WaitForSeconds(3);
+        Lights1.SetActive(true);
+        yield return new WaitForSeconds(3);
+        Lights2.SetActive(true);
+        yield return new WaitForSeconds(2);
+        Lights3.SetActive(true);
+        yield return new WaitForSeconds(5);
+        Jets.SetActive(true);
+        Title.SetActive(true);
+        Shuttle.GetComponent<ShuttleUp>().enabled = true;
+        yield return new WaitForSeconds(3);
+        Jets.SetActive(false);
+
+    }
+}
